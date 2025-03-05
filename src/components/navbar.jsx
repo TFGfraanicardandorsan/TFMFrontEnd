af9fdd90a0e8ada5243454da8a57778333d78028
FranAnillo <fraanicar@alum.us.es>
2025-03-05 23:44:44 +0100

    Redirección a mi Perfil

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "../styles/navbar-style.css"; // Importa los estilos
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell, faUser } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+    const navigate = useNavigate();
+
     return (
                 <nav className="navbar">
                     <ul className="nav-links">
@@ -14,8 +17,8 @@ export default function Navbar() {
                         <li><a href="/misIncidencias">Mis incidencias</a></li>
                     </ul>
                     <div className="nav-icons">
-                        <FontAwesomeIcon icon={faBell} className="icon bell-icon" />
-                        <FontAwesomeIcon icon={faUser} className="icon user" />
+                        <FontAwesomeIcon icon={faBell} className="icon bell-icon"/>
+                        <FontAwesomeIcon icon={faUser} className="icon user" onClick={() => navigate("/miPerfil")}/>
                     </div>
                 </nav>
     );
