1eebe5c18972dbb8220ee71b1dd60d973beeef71
FranAnillo <fraanicar@alum.us.es>
2025-03-08 18:56:06 +0100

    Protección de ruta de acceso: Ninguna

--- src/components/protectedRoute.jsx
+++ src/components/protectedRoute.jsx
@@ -1,15 +1,15 @@
-// components/ProtectedRoute.js
-import { Navigate } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
+// // components/ProtectedRoute.js
+// import { Navigate } from "react-router-dom";
+// //import useAuth from "../hooks/useAuth";
 
-const ProtectedRoute = ({ children }) => {
-    const isAuthenticated = useAuth();
+// const ProtectedRoute = ({ children }) => {
+//     const isAuthenticated = useAuth();
 
-    if (!isAuthenticated) {
-        return <Navigate to="/login" />;
-    }
+//     if (!isAuthenticated) {
+//         return <Navigate to="/login" />;
+//     }
 
-    return children;
-};
+//     return children;
+// };
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
