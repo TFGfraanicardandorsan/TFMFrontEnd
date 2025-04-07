c6c064bb586c60cfeca22d6e6fe738b63efa2cc5
dandorsan <dandorsan@alum.us.es>
2025-04-07 21:25:20 +0200

    navbar con mis notificaciones

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "../styles/navbar-style.css"; // Importa los estilos
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell, faUser } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router-dom";
+import {obtenerNotificaciones} from "../services/notificacion.js";
 
 export default function Navbar() {
 
@@ -10,6 +11,7 @@ export default function Navbar() {
     
     const [sidebarVisible, setSidebarVisible] = useState(false);
     const [notificaciones, setNotificaciones] = useState([]);
+    const [cargando, setCargando] = useState(true);
 
     useEffect(() => {
         const cargarNotificaciones = async () => {
@@ -39,7 +41,12 @@ export default function Navbar() {
     const cerrarSidebar = () => {
         setSidebarVisible(false);
     };
+
+    if (cargando) {
+        return <div className="loading-text">Cargando...</div>;
+      }
     return (
+
                 <nav className="navbar">
                     <ul className="nav-links">
                         <li><a href="/">Inicio</a></li>
@@ -49,7 +56,23 @@ export default function Navbar() {
                         <li><a href="/misIncidencias">Mis incidencias</a></li>
                     </ul>
                     <div className="nav-icons">
-                        <FontAwesomeIcon icon={faBell} className="icon bell-icon"/>{sidebarVisible && <Sidebar notificaciones={fakeNotificaciones} cerrarSidebar={cerrarSidebar} />}
+                        <FontAwesomeIcon icon={faBell} className="icon bell-icon"/>{mostrarSidebar()}
+                        {sidebarVisible && (
+                            <div className="sidebar">
+                                <button className="close-btn" onClick={cerrarSidebar}>X</button>
+                                <h2>Notificaciones</h2>
+                                {notificaciones.length > 0 ? (
+                                    notificaciones.map((notificacion, index) => (
+                                        <div key={index} className="notification-item">
+                                            <p>{notificacion.texto}</p>
+                                            <p>{notificacion.fecha}</p>
+                                        </div>
+                                    ))
+                                ) : (
+                                    <p>No hay notificaciones</p>
+                                )}
+                            </div>
+                        )}
                         <FontAwesomeIcon icon={faUser} className="icon user" onClick={() => navigate("/miPerfil")}/>
                     </div>
                 </nav>
