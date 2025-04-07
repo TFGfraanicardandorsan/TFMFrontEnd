0ef53c1054e15fddbec936ee09492ab1b72ba42e
FranAnillo <=>
2025-04-07 22:03:15 +0200

    fix

--- src/components/barraNotificaciones.jsx
+++ src/components/barraNotificaciones.jsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
-import "../styles/barraNotificacion-style.css"; // Asegúrate de tener un archivo CSS con los estilos para la barra lateral
+import "../styles/barraNotificacion-style.css";
 
-export default function barraNotificacion({ notificaciones, cerrarSidebar }) {
+export default function BarraNotificacion({ notificaciones, cerrarSidebar }) {
     return (
-        <div className="sidebar">
-            <button className="close-btn" onClick={cerrarSidebar}>X</button>
-            <h2>Notificaciones</h2>
-            <div className="notificaciones-list">
-                {notificaciones.map((notificacion) => (
-                    <div className="notificacion-card" key={notificacion.id}>
-                        <div className="notificacion-contenido">
-                            <h3>{notificacion.contenido}</h3>
-                            <p>{notificacion.fecha}</p>
+        <div className="sidebar-rectangulo">
+            <div className="sidebar">
+                <button className="close-btn" onClick={cerrarSidebar}>X</button>
+                <h2>Notificaciones</h2>
+                <div className="notificaciones-list">
+                    {notificaciones.map((notificacion) => (
+                        <div className="notificacion-card" key={notificacion.id}>
+                            <div className="notificacion-contenido">
+                                <h3>{notificacion.contenido}</h3>
+                                <p>{notificacion.fecha_creacion}</p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
