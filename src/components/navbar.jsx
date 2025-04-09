a994709972cbc74f97f534d3358c7702932ac7e9
dandorsan <dandorsan@alum.us.es>
2025-04-09 19:08:04 +0200

    nuevo estilo de mis notificaciones

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,89 +1,104 @@
 import { useState, useEffect } from "react";
 import "../styles/navbar-style.css"; // Importa los estilos
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faUser } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-import {obtenerNotificaciones} from "../services/notificacion.js";
+import { obtenerNotificaciones } from "../services/notificacion.js";
 
 const formatearFecha = (fechaISO) => {
-    const fecha = new Date(fechaISO);
-    
-    // Extraemos los componentes de la fecha
-    const dia = String(fecha.getDate()).padStart(2, '0');
-    const mes = String(fecha.getMonth() + 1).padStart(2, '0'); // Los meses en JavaScript comienzan desde 0
-    const anio = fecha.getFullYear();
-    const horas = String(fecha.getHours()).padStart(2, '0');
-    const minutos = String(fecha.getMinutes()).padStart(2, '0');
+  const fecha = new Date(fechaISO);
 
-    // Devolvemos la fecha formateada
-    return `${dia}/${mes}/${anio} ${horas}:${minutos}`;
+  // Extraemos los componentes de la fecha
+  const dia = String(fecha.getDate()).padStart(2, "0");
+  const mes = String(fecha.getMonth() + 1).padStart(2, "0"); // Los meses en JavaScript comienzan desde 0
+  const anio = fecha.getFullYear();
+  const horas = String(fecha.getHours()).padStart(2, "0");
+  const minutos = String(fecha.getMinutes()).padStart(2, "0");
+
+  // Devolvemos la fecha formateada
+  return `${dia}/${mes}/${anio} ${horas}:${minutos}`;
 };
 
 export default function Navbar() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-    
-    const [sidebarVisible, setSidebarVisible] = useState(false);
-    const [notificaciones, setNotificaciones] = useState([]);
-    const [cargando, setCargando] = useState(true);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [notificaciones, setNotificaciones] = useState([]);
+  const [cargando, setCargando] = useState(true);
 
-    useEffect(() => {
-        const cargarNotificaciones = async () => {
-            try {
-                const data = await obtenerNotificaciones();
-                if (Array.isArray(data.result.result)) {
-                    setNotificaciones(data.result.result);
-                } else {
-                    console.error("La respuesta no es un arreglo", data);
-                }
-            } catch (error) {
-                console.error("Error al obtener las notificaciones:", error);
-            } finally {
-                setCargando(false);
-            }
-        };
-        cargarNotificaciones();
-    }, []);
-    const mostrarSidebar = () => {
-        setSidebarVisible(true);
-    };
-
-    const cerrarSidebar = () => {
-        setSidebarVisible(false);
-    };
-
-    if (cargando) {
-        return <div className="loading-text">Cargando...</div>;
+  useEffect(() => {
+    const cargarNotificaciones = async () => {
+      try {
+        const data = await obtenerNotificaciones();
+        if (Array.isArray(data.result.result)) {
+          setNotificaciones(data.result.result);
+        } else {
+          console.error("La respuesta no es un arreglo", data);
+        }
+      } catch (error) {
+        console.error("Error al obtener las notificaciones:", error);
+      } finally {
+        setCargando(false);
       }
-    return (
-                <nav className="navbar">
-                    <ul className="nav-links">
-                        <li><a href="/">Inicio</a></li>
-                        <li><a href="#">Permutas</a></li>
-                        <li><a href="#">Solicitar Permutas</a></li>
-                        <li><a href="#">Mis Permutas</a></li>
-                        <li><a href="/misIncidencias">Mis incidencias</a></li>
-                    </ul>
-                    <div className="nav-icons">
-                        <FontAwesomeIcon icon={faBell} className="icon bell-icon" onClick={mostrarSidebar}/>
-                        {sidebarVisible && (
-                            <div className="sidebar">
-                                <button className="close-btn" onClick={cerrarSidebar}>X</button>
-                                <h2>Notificaciones</h2>
-                                {notificaciones.length > 0 ? (
-                                    notificaciones.map((notificacion) => (
-                                        <div key={notificacion.id} className="notification-item">
-                                            <p>{notificacion.contenido}</p>
-                                            <p>{formatearFecha(notificacion.fecha_creacion)}</p>
-                                        </div>
-                                    ))
-                                ) : (
-                                    <p>No hay notificaciones</p>
-                                )}
-                            </div>
-                        )}
-                        <FontAwesomeIcon icon={faUser} className="icon user" onClick={() => navigate("/miPerfil")}/>
-                    </div>
-                </nav>
-    );
+    };
+    cargarNotificaciones();
+  }, []);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  }
+
+  if (cargando) {
+    return <div className="loading-text">Cargando...</div>;
+  }
+  return (
+    <>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="#">Permutas</a>
+          </li>
+          <li>
+            <a href="#">Solicitar Permutas</a>
+          </li>
+          <li>
+            <a href="#">Mis Permutas</a>
+          </li>
+          <li>
+            <a href="/misIncidencias">Mis incidencias</a>
+          </li>
+        </ul>
+        <div className="nav-icons">
+          <FontAwesomeIcon
+            icon={faBell}
+            className="icon bell-icon"
+            onClick={toggleSidebar}
+          />
+          <FontAwesomeIcon
+            icon={faUser}
+            className="icon user"
+            onClick={() => navigate("/miPerfil")}
+          />
+        </div>
+      </nav>
+      {sidebarVisible && (
+        <div className="sidebar">
+          <h2>Notificaciones</h2>
+          {notificaciones.length > 0 ? (
+            notificaciones.map((notificacion) => (
+              <div key={notificacion.id} className="notification-item">
+                <p className="contenido">{notificacion.contenido}</p>
+                <p className="fecha">{formatearFecha(notificacion.fecha_creacion)}</p>
+              </div>
+            ))
+          ) : (
+            <p>No hay notificaciones</p>
+          )}
+        </div>
+      )}
+    </>
+  );
 }
