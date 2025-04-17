2c4e08d064a7cf34aaa1ef7c50d67d3ec5ab60c5
dandorsan <dandorsan@alum.us.es>
2025-04-17 16:54:41 +0200

    añadido el id, para identificar cada elemento del array

--- src/components/misIncidencias.jsx
+++ src/components/misIncidencias.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/misIncidencias-style.css";
 import Footer from "./footer";
 import Navbar from "./navbar";
@@ -6,67 +6,66 @@ import { useNavigate } from "react-router-dom";
 import { obtenerIncidenciasAsignadasUsuario } from "../services/incidencia";
 
 export default function MisIncidencias() {
-    const navigate = useNavigate();
-    const [incidencias, setIncidencias] = useState([]);
-    const [cargando, setCargando] = useState(true);
+  const navigate = useNavigate();
+  const [incidencias, setIncidencias] = useState([]);
+  const [cargando, setCargando] = useState(true);
 
-    useEffect(() => {
-        const cargarIncidencias = async () => {
-            try {
-                const data = await obtenerIncidenciasAsignadasUsuario();
-                console.log(data);
-                // Asegúrate de que data es un arreglo
-                if (Array.isArray(data.result.result)) {
-                    setIncidencias(data.result.result);
-                } else {
-                    console.error("La respuesta no es un arreglo", data);
-                    setIncidencias([]); // Si no es un arreglo, se puede establecer un arreglo vacío
-                }
-            } catch (error) {
-                console.error("Error al obtener las incidencias:", error);
-            } finally {
-                setCargando(false);
-            }
-        };
-    
-        cargarIncidencias();
-    }, []);
-    
+  useEffect(() => {
+    const cargarIncidencias = async () => {
+      try {
+        const data = await obtenerIncidenciasAsignadasUsuario();
+        setIncidencias(data.result.result);
+      } catch (error) {
+        console.error("Error al obtener las incidencias:", error);
+      } finally {
+        setCargando(false);
+      }
+    };
+    cargarIncidencias();
+  }, []);
 
-    return (
-        <>
-            <Navbar />
-            <div className="container">
-                <div className="header">
-                    <h1>Mis Incidencias</h1>
-                </div>
-
-                {cargando ? (
-                    <p className="loading-message">Cargando incidencias...</p>
-                ) : incidencias.length === 0 ? (
-                    <div className="no-incidencias">
-                        <p>No tienes incidencias registradas.</p>
-                        <button 
-                            onClick={() => navigate("/reportarIncidencia")} 
-                            className="big-button"
-                        >
-                            Solicitar Incidencia
-                        </button>
-                    </div>
-                ) : (
-                    <div className="incidencias-container">
-                        {incidencias.map((incidencia) => (
-                            <div key={incidencia.id} className="incidencia-card">
-                                <p><strong>Incidencia:</strong> {incidencia.fecha_creacion}</p>
-                                <p><strong>Tipo de Incidencia:</strong> {incidencia.tipo_incidencia}</p>
-                                <p><strong>Estado:</strong> {incidencia.estado_incidencia}</p>
-                                <p><strong>Comentario:</strong> {incidencia.descripcion}</p>
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="header">
+          <h1>Mis Incidencias</h1>
+        </div>
+        {cargando ? (
+          <p className="loading-message">Cargando incidencias...</p>
+        ) : incidencias.length === 0 ? (
+          <div className="no-incidencias">
+            <p>No tienes incidencias registradas.</p>
+            <button
+              onClick={() => navigate("/reportarIncidencia")}
+              className="big-button"
+            >
+              Solicitar Incidencia
+            </button>
+          </div>
+        ) : (
+          <div className="incidencias-container">
+            {incidencias.map((incidencia) => (
+              <div key={incidencia.id} className="incidencia-card">
+                <p>
+                  <strong>Incidencia:</strong> {incidencia.fecha_creacion}
+                </p>
+                <p>
+                  <strong>Tipo de Incidencia:</strong>{" "}
+                  {incidencia.tipo_incidencia}
+                </p>
+                <p>
+                  <strong>Estado:</strong> {incidencia.estado_incidencia}
+                </p>
+                <p>
+                  <strong>Comentario:</strong> {incidencia.descripcion}
+                </p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <Footer />
+    </>
+  );
 }
