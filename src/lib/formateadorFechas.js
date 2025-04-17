3c301675f8cdc28afd45eaf21c091f9fe2ee821e
dandorsan <dandorsan@alum.us.es>
2025-04-17 17:49:36 +0200

    css mejorado

--- src/lib/formateadorFechas.js
+++ src/lib/formateadorFechas.js
@@ -6,6 +6,7 @@ export const formatearFecha = (fechaISO) => {
       year: "numeric",
       hour: "2-digit",
       minute: "2-digit",
-    }).format(fecha);
+    }).format(fecha)
+      .replace(",", "");
   };
   
