4404e2ea0111117309d6af47c12cf1c80648fe70
dandorsan <dandorsan@alum.us.es>
2025-04-21 00:46:40 +0200

    eliminando fallo

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -3,17 +3,17 @@ import AuthContext from "./AuthContext.jsx";
 import PropTypes from "prop-types";
 import { obtenerSesion } from "../services/login.js";
 
-const comprobarSiHayCookies = document.cookie.includes("connect.sid");
+// const comprobarSiHayCookies = document.cookie.includes("connect.sid");
 
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({ loading: true,isAuthenticated: false,user: null });
     
     useEffect(() => {
         const apiObtenerSesion = async () => {
-            if (!comprobarSiHayCookies) {
-                setAuth({ loading: false, isAuthenticated: false, user: null });
-                return;
-            }
+            // if (!comprobarSiHayCookies) {
+            //     setAuth({ loading: false, isAuthenticated: false, user: null });
+            //     return;
+            // }
             try {
                 const data = await obtenerSesion();
                 setAuth({ loading: false,isAuthenticated: data.isAuthenticated,user: data.user });
