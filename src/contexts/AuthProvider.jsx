8ea2bbc3d73c95b96fb8a6bc425d02a165f044f4
dandorsan <dandorsan@alum.us.es>
2025-04-21 00:41:51 +0200

    comprobarSiHayCookies

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -3,20 +3,24 @@ import AuthContext from "./AuthContext.jsx";
 import PropTypes from "prop-types";
 import { obtenerSesion } from "../services/login.js";
 
+const comprobarSiHayCookies = document.cookie.includes("connect.sid");
+
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({ loading: true,isAuthenticated: false,user: null });
     
     useEffect(() => {
         const apiObtenerSesion = async () => {
+            if (!comprobarSiHayCookies) {
+                setAuth({ loading: false, isAuthenticated: false, user: null });
+                return;
+            }
             try {
                 const data = await obtenerSesion();
                 setAuth({ loading: false,isAuthenticated: data.isAuthenticated,user: data.user });
             } catch (error) {
                 if (error.response?.status === 401) {
                   setAuth({ loading: false, isAuthenticated: false, user: null });
-                } else {
-                    console.error("Error real al obtener la sesión:", error); 
-                  }
+                }
             }
         };
         apiObtenerSesion();
