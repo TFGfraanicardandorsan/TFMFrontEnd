0d55a696804ce9391ee85babb9b43728f118b69e
dandorsan <dandorsan@alum.us.es>
2025-04-20 22:52:40 +0200

    instalada dependencia cookie

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,52 @@
-import './App.css';
-import {Routes, Route, /*Navigate*/} from "react-router-dom";
-import Login from './components/login.jsx'
-import Home from './components/home.jsx'
-import Unauthorized from './components/Unauthorized.jsx'
-import ReportarIncidencia from './components/reportarIncidencia.jsx'
-import MisIncidencias from './components/misIncidencias.jsx'
-import MiPerfil from './components/miPerfil.jsx'
-import SeleccionarEstudio from './components/seleccionarEstudio.jsx'
-import SeleccionarAsignatura from './components/selectorAsignatura.jsx'
-import Incidencias from './components/incidencias.jsx'
-import NotFound from './components/notFound.jsx'
-import GeneracionPDF from './components/generacionPDF.jsx';
-import SolicitarGrupos from './components/seleccionarGrupos.jsx'; 
-import PoliticaPrivacidad from './components/politicaPrivacidad.jsx';
-import CrearNotificacion from './components/crearNotificacion.jsx';
-import SolicitarPermuta from './components/solicitarPermuta.jsx';
-import Cookies from './components/cookies.jsx';
-import MisSolicitudesPermuta from './components/misPermutas.jsx';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { ProtectedRoute } from "./routes/ProtectedRoute.jsx";
+import { RoleRoute } from "./routes/RoleRoute.jsx";
+import Login from "./components/login.jsx";
+import Home from "./components/home.jsx";
+import Unauthorized from "./components/Unauthorized.jsx";
+import NotFound from "./components/notFound.jsx";
+import ReportarIncidencia from "./components/reportarIncidencia.jsx";
+import MisIncidencias from "./components/misIncidencias.jsx";
+import MiPerfil from "./components/miPerfil.jsx";
+import SeleccionarEstudio from "./components/seleccionarEstudio.jsx";
+import SeleccionarAsignatura from "./components/selectorAsignatura.jsx";
+import Incidencias from "./components/incidencias.jsx";
+import GeneracionPDF from "./components/generacionPDF.jsx";
+import SolicitarGrupos from "./components/seleccionarGrupos.jsx";
+import PoliticaPrivacidad from "./components/politicaPrivacidad.jsx";
+import CrearNotificacion from "./components/crearNotificacion.jsx";
+import SolicitarPermuta from "./components/solicitarPermuta.jsx";
+import Cookies from "./components/cookies.jsx";
+import MisSolicitudesPermuta from "./components/misPermutas.jsx";
 export function App() {
-
   return (
-    <>
-      <Routes>
-        <Route path='/login' element={<Login/>} />
-        <Route path="*" element={<NotFound />} />
-        <Route path='/unauthorized' element={<Unauthorized />} />
-        <Route path='/' element={<Home/>} />
-        <Route path='/reportarIncidencia' element={<ReportarIncidencia/>} />
-        <Route path='/misIncidencias' element={<MisIncidencias/>} />
-        <Route path='/miPerfil' element={<MiPerfil/>} />
-        <Route path='/seleccionarEstudios' element={<SeleccionarEstudio/>} />
-        <Route path='/seleccionarAsignaturas' element={<SeleccionarAsignatura/>} />
-        <Route path='/incidencias' element={<Incidencias/>} />
-        <Route path='/generarPermuta' element={<GeneracionPDF/>} />
-        <Route path = '/seleccionarGrupos' element={<SolicitarGrupos/>} />
-        <Route path = '/politicaPrivacidad' element={<PoliticaPrivacidad/>} />
-        <Route path= '/crearNotificacion' element={<CrearNotificacion/>} />
-        <Route path='/solicitarPermuta' element={<SolicitarPermuta/>} />
-        <Route path='/cookies' element={<Cookies/>} />
-        <Route path='/misSolicitudesPermuta' element={<MisSolicitudesPermuta/>} />
-      </Routes>
-    </>
-  )
-}
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/unauthorized" element={<Unauthorized />} />
+          <Route path="*" element={<NotFound />} />
 
-export default App
+          <Route path="/" element={<Home />} />
+          <Route path="/reportarIncidencia" element={<ReportarIncidencia />} />
+          <Route path="/misIncidencias" element={<MisIncidencias />} />
+          <Route path="/miPerfil" element={<MiPerfil />} />
+          <Route path="/seleccionarEstudios" element={<SeleccionarEstudio />} />
+          <Route path="/seleccionarAsignaturas" element={<SeleccionarAsignatura />}/>
+          <Route path="/incidencias" element={<Incidencias />} />
+          <Route path="/generarPermuta" element={<GeneracionPDF />} />
+          <Route path="/seleccionarGrupos" element={<SolicitarGrupos />} />
+          <Route path="/politicaPrivacidad" element={<PoliticaPrivacidad />} />
+          <Route path="/crearNotificacion" element={<CrearNotificacion />} />
+          <Route path="/solicitarPermuta" element={<SolicitarPermuta />} />
+          <Route path="/cookies" element={<Cookies />} />
+          <Route
+            path="/misSolicitudesPermuta"
+            element={<MisSolicitudesPermuta />}
+          />
+        </Routes>
+      </AuthProvider>
+  );
+}
+export default App;
