9b54ccffe14ea4a58071f18f9caedc592734435b
dandorsan <dandorsan@alum.us.es>
2025-04-21 00:52:37 +0200

    comprobarSiHayCookies

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -3,17 +3,16 @@ import AuthContext from "./AuthContext.jsx";
 import PropTypes from "prop-types";
 import { obtenerSesion } from "../services/login.js";
 
-// const comprobarSiHayCookies = document.cookie.includes("connect.sid");
-
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({ loading: true,isAuthenticated: false,user: null });
     
     useEffect(() => {
         const apiObtenerSesion = async () => {
-            // if (!comprobarSiHayCookies) {
-            //     setAuth({ loading: false, isAuthenticated: false, user: null });
-            //     return;
-            // }
+            const comprobarSiHayCookies = document.cookie.includes("connect.sid");
+            if (!comprobarSiHayCookies) {
+                setAuth({ loading: false, isAuthenticated: false, user: null });
+                return;
+            }
             try {
                 const data = await obtenerSesion();
                 setAuth({ loading: false,isAuthenticated: data.isAuthenticated,user: data.user });
--- src/routes/RoleRoute.jsx
+++ src/routes/RoleRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "../hooks/useAuth.js";
 import PropTypes from "prop-types";
 
 export const RoleRoute = ({ children, allowedRoles }) => {
