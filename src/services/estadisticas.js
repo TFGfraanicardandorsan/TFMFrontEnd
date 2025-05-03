f62a9b62218dea601f114bac3412b1e16a223f8c
dandorsan <dandorsan@alum.us.es>
2025-05-03 21:09:16 +0200

    api estadísiticas

--- src/services/estadisticas.js
+++ src/services/estadisticas.js
@@ -1,9 +1,9 @@
 import { postAPI } from "../lib/methodAPIs.js";
 
 export const obtenerEstadisticasPermutas = async () => {
-    return await postAPI("/api/v1/admin/estadisticas/permutas");
+    return await postAPI("/api/v1/estadisticas/permutas");
 };
 
 export const obtenerEstadisticasSolicitudes = async () => {
-    return await postAPI("/api/v1/admin/estadisticas/solicitudes");
+    return await postAPI("/api/v1/estadisticas/solicitudes");
 };
