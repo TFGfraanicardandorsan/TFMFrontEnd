bfb2097e5a8099924c778b3977fe3c137f2a6301
dandorsan <dandorsan@alum.us.es>
2025-05-04 03:21:08 +0200

    roleRoute

--- src/routes/RoleRoute.jsx
+++ src/routes/RoleRoute.jsx
@@ -5,7 +5,8 @@ import PropTypes from "prop-types";
 export const RoleRoute = ({ children, allowedRoles }) => {
   const { loading, isAuthenticated, user } = useAuth();
   if (loading) return <div>Cargando...</div>;
-  if (!isAuthenticated || !allowedRoles.includes(user?.rol)) return <Navigate to="/unauthorized" replace />;
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
+  if (!allowedRoles.includes(user?.rol)) return <Navigate to="/unauthorized" replace />;
   return children;
 };
 RoleRoute.propTypes = { children: PropTypes.node.isRequired,allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired };
