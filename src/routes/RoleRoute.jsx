467f6f9eca50841a0d040fcc42e8bebf76892757
dandorsan <dandorsan@alum.us.es>
2025-05-04 20:35:11 +0200

    home

--- src/routes/RoleRoute.jsx
+++ src/routes/RoleRoute.jsx
@@ -1,30 +1,12 @@
-import { useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth.js";
 import PropTypes from "prop-types";
 
 export const RoleRoute = ({ children, allowedRoles }) => {
   const { loading, isAuthenticated, user } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (loading) return;
-    if (!isAuthenticated) {
-      return <Navigate to="/login" replace />;
-    }
-    if (!allowedRoles.includes(user?.rol)) {
-      return <Navigate to="/unauthorized" replace />;
-    }
-    if (user?.rol === "administrador") {
-      navigate("/admin");
-    } else if (user?.rol === "estudiante") {
-      navigate("/estudiante");
-    }
-  }, [loading, isAuthenticated, user, allowedRoles, navigate]);
-
   if (loading) return <div>Cargando...</div>;
-
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
+  if (!allowedRoles.includes(user?.rol)) return <Navigate to="/unauthorized" replace />;
   return children;
 };
-
 RoleRoute.propTypes = { children: PropTypes.node.isRequired,allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired };
