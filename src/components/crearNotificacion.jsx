e3ef0970b03e49c89def5e7fdf2afefa02d304e5
FranAnillo <=>
2025-05-11 12:01:37 +0200

    Agregar importación de estilos en CrearNotificacion

--- src/components/crearNotificacion.jsx
+++ src/components/crearNotificacion.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { crearNotificacion } from "../services/notificacion";
+import "./../styles/crearNotificacion.css";
 
 export default function CrearNotificacion() {
     const [contenido, setContenido] = useState("");
