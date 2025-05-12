e4dca6c5e8e3cf5d19650f9293084e6359e096a8
dandorsan <dandorsan@alum.us.es>
2025-05-12 18:14:27 +0200

    worker pdf

--- src/components/generacionPDF.jsx
+++ src/components/generacionPDF.jsx
@@ -22,7 +22,7 @@ import {
   validarTelefono,
 } from "../lib/validadores.js";
 import { Document, Page, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = "../../public/pdf.worker.min.mjs"
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
 
 export default function GeneracionPDF() {
   const [dni, setDni] = useState("");
