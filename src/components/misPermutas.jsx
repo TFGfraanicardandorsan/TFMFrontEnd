8c91dbeaf966f20e16241f1a9c1f6c4bbb68fb8d
Francisco Anillo <fraanicar@alum.us.es>
2025-05-15 10:11:39 +0200

    Add Swiper modules for navigation and pagination in MisPermutas component

--- src/components/misPermutas.jsx
+++ src/components/misPermutas.jsx
@@ -1,5 +1,7 @@
+"use client;"
 import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
