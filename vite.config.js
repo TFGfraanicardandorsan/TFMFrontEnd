51da3218dbe6159ab8bf87bb8345e8815c4c08d2
dandorsan <dandorsan@alum.us.es>
2025-05-20 18:17:58 +0200

    vite config actualizado

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-server: {
+  server: {
     port: 3033,
     host: true,
-       allowedHosts: ['permutas.eii.us.es']
-}
-
+    allowedHosts: ['permutas.eii.us.es']
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild'
+  }
 })
-
-
-
-
