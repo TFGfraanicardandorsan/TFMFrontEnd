8c3b2af986ac79f72fb37157eaab459661dff88e
Francisco José Anillo Carrasco <fraanicar@alum.us.es>
2026-01-13 13:45:58 +0100

    fix carpeta

--- src/components/administrador/crearNotificacion.jsx
+++ src/components/administrador/crearNotificacion.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { crearNotificacion } from "../../services/notificacion";
-import "./../styles/crearNotificacion.css";
+import "../../styles/crearNotificacion.css";
 import { toast } from "react-toastify";
 import { logError } from "../../lib/logger";
 
