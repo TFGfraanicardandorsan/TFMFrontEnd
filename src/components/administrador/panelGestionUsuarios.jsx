44907c9dafb45545cb26ec643ede354e164dac1a
Francisco Anillo <fraanicar@alum.us.es>
2026-01-25 18:09:59 +0100

    Devuelve undefined

--- src/components/administrador/panelGestionUsuarios.jsx
+++ src/components/administrador/panelGestionUsuarios.jsx
@@ -16,7 +16,7 @@ const UserManagementPanel = () => {
             try {
                 const response = await obtenerTodosUsuarios();
                 // Asegurarse de que la respuesta es un array
-                console.log(response.data);
+                console.log(response);
                 if (Array.isArray(response?.data.result)) {
                     // Mapear claves del backend a las esperadas por el frontend
                     const mappedUsers = response.data.map(u => ({
