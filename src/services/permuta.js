385c61a62514df422862c3f040984420aaf8eddd
Francisco José Anillo Carrasco <fraanicar@alum.us.es>
2026-09-11 10:34:43 +0200

    Actualización de retirar vigencia

--- src/services/permuta.js
+++ src/services/permuta.js
@@ -89,5 +89,5 @@ export const generarBorradorPermuta = async (IdsPermuta) => {
 }
 
 export const actualizarVigenciaPermutas = async () => {
-    return await postAPI("/api/v1/solicitudPermuta/actualizarVigenciaPermutas");
+    return await postAPI("/api/v1/permutas/actualizarVigenciaPermutas");
 }
